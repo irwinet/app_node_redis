be71b6effb9860799957fc6d628f1a8108e2352a
Irwin Estrada <ext_iestrada@tottus.com.pe>
2022-07-18 22:29:26 -0500

    Add Rest-Client

--- src/index.js
+++ src/index.js
@@ -16,28 +16,58 @@ app.use(respondeTime());
 
 app.get('/character', async (req, res) => {
 
-    await client.connect();
+    try {
+        await client.connect();
 
-    const characters = await client.get('characters');
-    if(characters)
-    {
-        // console.log(characters);  
+        const characters = await client.get('characters');
+        if (characters) {
+            // console.log(characters);  
+            await client.disconnect();
+            return res.json(JSON.parse(characters));
+        }
+
+        const response = await axios.get('https://rickandmortyapi.com/api/character');
+        // console.log(response.data);
+
+        //await client.connect();
+
+        await client.set('characters', JSON.stringify(response.data));
+        await client.expire('characters', 120);
+
         await client.disconnect();
-        return res.json(JSON.parse(characters));
-    }      
 
-    const response = await axios.get('https://rickandmortyapi.com/api/character');
-    // console.log(response.data);
+        res.json(response.data);
+    } catch (error) {
+        console.log(error);
+    }
 
-    //await client.connect();
+});
 
-    await client.set('characters', JSON.stringify(response.data));
-    await client.expire('characters', 120);
+app.get('/character/:id', async (req, res) => {
+    try {
+        await client.connect();
 
-    await client.disconnect();
+        const character = await client.get(req.params.id);
+        if (character) {
+            // console.log(character);  
+            await client.disconnect();
+            return res.json(JSON.parse(character));
+        }
 
-    res.json(response.data);
+        //const characters = await client.get('characters');
+        const response = await axios.get('https://rickandmortyapi.com/api/character/' + req.params.id);
 
+        await client.set(req.params.id, JSON.stringify(response.data));
+        await client.expire(req.params.id, 120);
+
+        await client.disconnect();
+
+        res.json(response.data);
+    } catch (error) {
+        console.log(error);
+        await client.disconnect();
+        return res.status(error.response.status).json({ message: error.message });
+    }
 });
 
 app.listen(3000);
